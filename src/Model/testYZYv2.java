9c1f4e3c10cbb9df504133d0aa9461c949794891
Zongyou Yang <dryang0624@163.com>
2023-04-14 21:34:38 +0800

    1233

--- src/Model/testYZYv2.java
+++ src/Model/testYZYv2.java
@@ -6,9 +6,11 @@ public class testYZYv2 {
     public static void main(String[] args) {
         InfoReader infoReader = new InfoReader();
         infoReader.readit("src/Model/Info.txt");
+        //infoReader.readit("Info.txt");
         System.out.println(infoReader.getStudents());
         System.out.println(infoReader.getCourses());
         System.out.println();
+        TimeLineDisplay timeLineDisplay = new TimeLineDisplay(infoReader);
     }
 
 
